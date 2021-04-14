f4ce2c1d708f823acf598de61eda245745111d66
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-04-14 11:50:12 +0200

    test commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# Try to use pivot_table to process the input TSV as shown in the page below
+# https://stackoverflow.com/questions/46375147/create-new-columns-from-unique-row-values-in-a-pandas-dataframe
+# df = df.pivot_table(index=['acc'], columns=['feature'], values='content_fraction').fillna(0)
+print("hello world")
