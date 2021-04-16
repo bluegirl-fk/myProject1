2ca1088b63e8462687a8df2e9ef33e20001cc7ef
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-04-16 18:33:19 +0200

    last last last..!

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import itertools
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# TODO: explain each variable and abbreviations in the beginning, define your standard naming system
+### explain variables and stuff
 mobidb_features_lst = []  # setting the transversed dataframe's column names which are the feature/predictor names as
 # list values,#column names list contains dif features (dif predictors for dif plots, 78 features = 78 plots)
 mobidb_predictors_cont_fra_dict = {}  # dict with each feature as key and the associated list of content_fractions as
 # values
-mobidb_each_cont_fra_lst = []  # # content_fraction of homo sapiens list based on each feature,
+cont_fra_temp_lst = []  # # content_fraction of homo sapiens list based on each feature,
 # it changes with the for loop iteration
 
 
@@ -25,7 +25,7 @@ def drawplot(plot_input_lst, bins_amount, isDense, xLabel, yLabel, png_file_name
     plt.savefig(png_file_name + file_format)
 
 
-### import your files
+### import files
 mobidb_original_df = pd.read_csv('data/mobidb_result.tsv', sep='\t')
 # transpose the mobidb_original_df and keep only the necessary data
 mobidb_transposed_df = mobidb_original_df.pivot_table(index=['acc'], columns=['feature'],
@@ -36,8 +36,8 @@ mobidb_features_lst = mobidb_transposed_df.columns.str.split(',').tolist()
 mobidb_features_lst = list(itertools.chain(*mobidb_features_lst))  # list of lists to a flat list
 # get each column's content as a list and set it as dict value (keys are predictor's name(feature's name))
 for each_feature in mobidb_features_lst:
-    mobidb_each_cont_fra_lst = mobidb_transposed_df[each_feature].tolist()
-    mobidb_predictors_cont_fra_dict[each_feature] = mobidb_each_cont_fra_lst
+    cont_fra_temp_lst = mobidb_transposed_df[each_feature].tolist()
+    mobidb_predictors_cont_fra_dict[each_feature] = cont_fra_temp_lst
 ### draw each plot
 for each_feature_name in mobidb_features_lst:
     drawplot(mobidb_predictors_cont_fra_dict[each_feature_name], 20, True, each_feature_name, "Protein Count",
