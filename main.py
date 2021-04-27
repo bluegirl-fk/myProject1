9e3f97e3d2197abe9eaa7a697fd66d6e6d249cd9
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-04-27 17:02:35 +0200

    more organized 3d matrix, del comments

--- main.py
+++ main.py
@@ -6,18 +6,13 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-### explain variables and stuff
-mobidb_features_lst = []  # setting the transversed dataframe's column names which are the feature/predictor names as
-# list values,#column names list contains dif features (dif predictors for dif plots, 78 features = 78 plots)
-mobidb_predictors_cont_fra_dict = {}  # dict with each feature as key and the associated list of content_fractions as
-# values
-cont_fra_temp_lst = []  # content_fraction of homo sapiens list based on each feature, it changes with the for loop iteration
+mobidb_features_lst = []
+mobidb_predictors_cont_fra_dict = {}
+cont_fra_temp_lst = []
 
-disease_cont_fra_temp_lst = []  # content_fraction of disease acc list based on each feature, it changes with the for loop iteration
-disease_predictors_cont_fra_dict = {}  # dict with each feature as key and the associated list of ONLY disease content_fractions as values
+disease_cont_fra_temp_lst = []
+disease_predictors_cont_fra_dict = {}
 
-
-### methods
 def drawplot(plot_input_lst, bins, is_dense, x_label, y_label, png_file_name, subdirectory):
     plt.hist(plot_input_lst, bins=bins,
              density=is_dense)
@@ -40,8 +35,7 @@ def compare_plot(first_lst, second_lst, bins, is_dense, first_label, second_labe
 
 
 if __name__ == '__main__':
-    ### Dataframe files import and manipulation
-
+    ### Files import and manipulation
     mobidb_original_df = pd.read_csv('data/mobidb_result.tsv', sep='\t')
     # transpose the mobidb_original_df and keep only the necessary data
     mobidb_transposed_df = mobidb_original_df.pivot_table(index=['acc'], columns=['feature'],
@@ -63,28 +57,28 @@ if __name__ == '__main__':
             # if disease_mobidb_matrix[i, j] != 0:
             k = int(round(disease_mobidb_matrix[i, j] * 10))
             matrix[i, j, k] = 1
-    pass
-    # mobidb_features_lst = list(itertools.chain(*mobidb_features_lst))  # list of lists to a flat list
-    # # get each column's content as a list and set it as dict value (keys are predictor's name(feature's name))
-    # for each_feature in mobidb_features_lst:
-    #     cont_fra_temp_lst = mobidb_transposed_df[each_feature].tolist()
-    #     mobidb_predictors_cont_fra_dict[each_feature] = cont_fra_temp_lst
-    # ### draw each plot of whole homo sapiens proteins with content fraction based on each feature
-    # # for each_feature in mobidb_features_lst[1:]: ##cuz the fist item is 'acc' and we don't need it for the plots, just need the content fraction
-    # #     drawplot(mobidb_predictors_cont_fra_dict[each_feature], 30, True, each_feature + '_homosapiens', "Protein Count(relative)",
-    # #              each_feature, 'homosapiens')
-    # ##plot for diseases
-    # for each_feature in mobidb_features_lst:
-    #     disease_cont_fra_temp_lst = disease_mobidb_df[each_feature].tolist()
-    #     disease_predictors_cont_fra_dict[each_feature] = disease_cont_fra_temp_lst
-    #
-    # # for each_feature in mobidb_features_lst[1:]:
-    # #      drawplot(disease_predictors_cont_fra_dict[each_feature], 30, False, each_feature + '_Disease', 'Protein count',each_feature+'_disease','disease')
-    #
-    # # ### comparative histogram
-    # # for each_feature in mobidb_features_lst[
-    # #                     1:]:  # cuz the fist item is 'acc' and we don't need it for the plots, just need the content fraction
-    # #     compare_plot(mobidb_predictors_cont_fra_dict[each_feature], disease_predictors_cont_fra_dict[each_feature], 30,
-    # #                  True, x_label=each_feature + '_comparison', y_label='proteins count(relative)', png_file_name=each_feature,
-    # #                  first_label= 'all_disordered_Pr.s', second_label= 'disease_Pr.s')
-    #
+
+    mobidb_features_lst = list(itertools.chain(*mobidb_features_lst))
+
+    for each_feature in mobidb_features_lst:
+        cont_fra_temp_lst = mobidb_transposed_df[each_feature].tolist()
+        mobidb_predictors_cont_fra_dict[each_feature] = cont_fra_temp_lst
+    ### Plot for homosapiens
+    for each_feature in mobidb_features_lst[1:]: ##cuz the fist item is 'acc' and we don't need it for the plots, just need the content fraction
+        drawplot(mobidb_predictors_cont_fra_dict[each_feature], 30, True, each_feature + '_homosapiens', "Protein Count(relative)",
+                 each_feature, 'homosapiens')
+    ##plot for diseases
+    for each_feature in mobidb_features_lst:
+        disease_cont_fra_temp_lst = disease_mobidb_df[each_feature].tolist()
+        disease_predictors_cont_fra_dict[each_feature] = disease_cont_fra_temp_lst
+
+    for each_feature in mobidb_features_lst[1:]:
+         drawplot(disease_predictors_cont_fra_dict[each_feature], 30, False, each_feature + '_Disease', 'Protein count',each_feature+'_disease','disease')
+
+    ### comparative histogram (homosapiens Vs. disease)
+    for each_feature in mobidb_features_lst[
+                        1:]:  # cuz the fist item is 'acc' and we don't need it for the plots, just need the content fraction
+        compare_plot(mobidb_predictors_cont_fra_dict[each_feature], disease_predictors_cont_fra_dict[each_feature], 30,
+                     True, x_label=each_feature + '_comparison', y_label='proteins count(relative)', png_file_name=each_feature,
+                     first_label= 'all_disordered_Pr.s', second_label= 'disease_Pr.s')
+
