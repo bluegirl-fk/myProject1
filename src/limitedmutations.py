1f66c8b4fa2f3b6b366fca17c111c23fb54066c7
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-07-30 11:20:18 +0200

    fist commit

---
+++ src/limitedmutations.py
@@ -0,0 +1,5 @@
+# the aim of this file is to process candidate genes file(7mb) and all de novo mutations(59.6mb) both from gene4dn
+# Release version 1.1 (Updated: 09/09/2020)
+# start date: July 30th 2021
+
+
