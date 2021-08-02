37280c13273f072405efe5aa0a75a89dc305a931
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-08-02 16:06:11 +0200

    initiation commit

---
+++ src/mobibool.py
@@ -0,0 +1,11 @@
+# this file is supposed to get mobidb+mutposition, is_in_startend and ndd bool, index*, and give a df in the outcome
+# with added columns: brain bool, ASD bool, EE bool, ID bool, SCZ bool
+# *index (mutual idx of mut_acc_merged_df and mutinfo subdf which is useful to merge mobidb & g4dn_mutinfo_acc,
+# could be useless now :/ not sure
+
+import pandas as pd
+import config as cfg
+import sys
+
+if __name__ == '__main__':
+    sys.exit()
