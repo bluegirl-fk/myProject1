a34d20d7babe0aa22476dbfab6f092cd67562eea
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-08-04 16:59:24 +0200

    changed var names

--- src/mobibool.py
+++ src/mobibool.py
@@ -15,27 +15,26 @@ import seaborn as sns
 
 
 if __name__ == '__main__':
-    df = pd.read_csv(cfg.data[''] + '/mobidb_result.tsv', sep='\t')
+    mobidb = pd.read_csv(cfg.data[''] + '/mobidb_result.tsv', sep='\t')
     ## brain proteins
     brain_prot_lst = bd.brain_pr_lst_generator()  # n: 8428
     brain_pr_df = DataFrame(brain_prot_lst, columns=['acc'])
     brain_pr_df['brain'] = 1
-    df = df.merge(brain_pr_df, how='left', on='acc')
-    # df = df.set_index("acc")
-
-    # ## NDD proteins, could also specify index_col= ..., and pass a list for multiple idxs
-    df_g4 = pd.read_csv(cfg.data['gene4'] + '/positive_cand_g4mobi_concat.csv', usecols=["acc_x", "Phenotype"])
-    df_g4 = df_g4.drop_duplicates()
-    df = df.merge(df_g4, how='left', left_on='acc', right_on='acc_x')
-    df = df.drop(columns=['start..end', 'acc_x'])
+    mobidb = mobidb.merge(brain_pr_df, how='left', on='acc')
+    ## NDD proteins, could also specify index_col= ..., and pass a list for multiple idxs
+    phen_df = pd.read_csv(cfg.data['gene4'] + '/positive_cand_g4mobi_concat.csv', usecols=["acc_x", "Phenotype"])
+    phen_df = phen_df.drop_duplicates()
+    ## merge to get Phenotypes column
+    mobidb = mobidb.merge(phen_df, how='left', left_on='acc', right_on='acc_x')
+    mobidb = mobidb.drop(columns=['start..end', 'acc_x'])
     # trying multi-level index here: from: https://www.youtube.com/watch?v=tcRGa2soc-c
     # TODO: in the end you should add the brain to the Phenotype column
     features_lst = ['prediction-disorder-mobidb_lite', 'prediction-low_complexity-merge',
                     'prediction-lip-anchor', 'homology-domain-merge']
-    midx = df[df.feature.isin(features_lst)]  # (194383, 7)
+    mobi_feature_df = mobidb[mobidb.feature.isin(features_lst)]  # (194383, 7)
 
-    ser = midx.groupby(['acc', 'feature', 'Phenotype']).content_fraction.mean()
-    ser_df = ser.unstack()
+    mobi_3d_series = mobi_feature_df.groupby(['acc', 'feature', 'Phenotype']).content_fraction.mean()
+    mobi_3d_df = mobi_3d_series.unstack()
     ## how to access the values
     # ser.loc['A6NHU9']
     # ser.loc['A6NHU9', 'prediction-lip-anchor']
@@ -60,26 +59,15 @@ if __name__ == '__main__':
     # plot.set_xticklabels(rotation=45, va="center", position=(0, -0.02))
     # plt.tight_layout()
     # plt.show()
-    #
+
     plt.figure(figsize=(12, 12))
     sns.set_theme(style="whitegrid")
     # Load the example tips dataset
     # Draw a nested violinplot and split the violins for easier comparison
-    plot = sns.violinplot(data=ser_df.loc[(slice(None), 'prediction-disorder-mobidb_lite'), :])
+    plot = sns.violinplot(data=mobi_3d_df.loc[(slice(None), 'prediction-disorder-mobidb_lite'), :])
     sns.set_style("ticks")
     # plot.set_xticklabels(labels=df['Phenotype'].unique().tolist(),rotation=45, va="center", position=(0, -0.02))
     plt.tight_layout()
     plt.show()
-    # # Damiano's code for multi index
-    # # df_g4['val'] = True
-    # # df_g4 = df_g4.set_index(["acc_x", "Phenotype"]).unstack()
-    # # df_g4 = df_g4.loc[:, 'val']
-    # # df_g4['ndd'] = True
-    # # df = df.merge(df_g4, how='left', left_on='acc', right_on='acc_x')
 
 
-    # # # disorder content
-    # # columns = ['acc', 'content_fraction', 'ndd', 'brain', 'ASD']
-    # # df_dc = df[df['feature'] == 'prediction-disorder-mobidb_lite'][columns]
-    # # df_dc = df_dc.set_index(['acc', 'content_fraction'])
-    # # df_dc[df_dc.notna()] = df_dc.index.get_level_values('content_fraction')
