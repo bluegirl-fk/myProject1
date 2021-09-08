27eaf63c91fe9f368c783d66573d077346418589
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-09-08 17:40:20 +0200

    before change

--- src/urlopener.py
+++ src/urlopener.py
@@ -4,11 +4,20 @@ import urllib.request
 
 url = 'https://www.uniprot.org/uniprot/Q14204.txt'
 file_test = urllib.request.urlopen(url)
-for l in file_test:  # TODO: include a condition that first checks if the DISEASE lines exist, decide what to store from that part
+for l in file_test:  # TODO: include a condition that first checks if the DISEASE lines exist, decide what to store from that part, check how many of your uniprot IDs remain based on this condition
 	line = l.decode("utf-8")
-	if line.startswith('FT') and 'VARIANT' in line:
-		print('There is a variant here!')
-		position = line.split('VARIANT')[1:][0].replace(' ', '')
-		print(position)
+	if line.startswith('CC') and '-!- DISEASE:' in line:
+		disease_name = line.split('CC   -!- DISEASE:')[1]
+		if '[' in line:
+			disease_name = disease_name.split('[')[0]
+			break
+		else:
+			line = line.next
+			disease_name = disease_name.append()
+		print(disease_name)
+		# if line.startswith('FT') and 'VARIANT' in line:
+		# 	print('There is a variant here!')
+		# 	position = line.split('VARIANT')[1:][0].replace(' ', '')
+		# 	print(position)
 
 	# print(decoded_line)
