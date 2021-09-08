17f5f86814ac3f0fd3387f198d65b3c0b4b35651
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-09-08 16:35:41 +0200

    getting mutation positions

--- src/urlopener.py
+++ src/urlopener.py
@@ -4,8 +4,11 @@ import urllib.request
 
 url = 'https://www.uniprot.org/uniprot/Q14204.txt'
 file_test = urllib.request.urlopen(url)
-for l in file_test:
+for l in file_test:  # TODO: include a condition that first checks if the DISEASE lines exist, decide what to store from that part
 	line = l.decode("utf-8")
 	if line.startswith('FT') and 'VARIANT' in line:
 		print('There is a variant here!')
+		position = line.split('VARIANT')[1:][0].replace(' ', '')
+		print(position)
+
 	# print(decoded_line)
