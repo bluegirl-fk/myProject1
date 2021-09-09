982ac15b307517fa49584575768cd30cfe2bf346
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-09-09 10:23:16 +0200

    minor change + del comment

--- src/urlopener.py
+++ src/urlopener.py
@@ -7,16 +7,16 @@ import re
 url = 'https://www.uniprot.org/uniprot/Q14204.txt'
 my_par = []
 file_test = urllib.request.urlopen(url)
-for l in file_test:  # TODO: include a condition that first checks if the DISEASE lines exist, decide what to store from that part, check how many of your uniprot IDs remain based on this condition
+for l in file_test:
 	line = l.decode("utf-8")
-	if ( re.search(r'^CC\s+-!-', line ) ):
-		if ( re.search(r'^CC\s+-!- DISEASE:', line) ):
+	if re.search(r'^CC\s+-!-', line):
+		if re.search(r'^CC\s+-!- DISEASE:', line):
 			# Crerate new paragraph
 			my_par.append(line)
 		# Otherwise, break only if all DISEASE line have been terminated
-		elif ( len(my_par) > 0 ):
+		elif len(my_par) > 0:
 			break
-	elif ( len(my_par) > 0 ):
+	elif len(my_par) > 0:
 		my_par[-1] += line
 
 print(my_par)
