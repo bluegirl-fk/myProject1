84e93c171fe15dfd4bc03bf86a1215e6aaa8daab
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-09-10 17:24:31 +0200

    Mobidb change new, Todos

--- src/urlopener.py
+++ src/urlopener.py
@@ -10,28 +10,33 @@ import brain as bd
 # my_proteins_acc_lst = set(my_proteins_acc_df['acc'].tolist())  # 1308
 # my_proteins_acc_lst = list(dict.fromkeys(my_proteins_acc_lst))
 # my_proteins_acc_lst = my_proteins_acc_lst[:3]
-mobidb = pd.read_csv(cfg.data[''] + '/mobidb_result.tsv', sep='\t')
+mobidb1 = pd.read_csv(cfg.data[''] + '/mobidb_result.tsv', sep='\t')
+# new tsv that Damiano sent based on NDDs that were not in mobidb and now they are gonna be added!
+mobidb2 = pd.read_csv(cfg.data['phens'] + '/ndd-not-in-mobidb-byDamiano.tsv', sep='\t')
+mobidb = mobidb1.append(mobidb2, ignore_index=True)
+mobidb.to_csv(cfg.data['phens'] + '/mobidb-results+ndd-tsv-damiano-shared.tsv')
 mobidb_pr_lst = mobidb['acc'].unique().tolist()
-# NDD , could also specify index_col= ..., and pass a list for multiple idxs
+new_mobidb_list = []
+with open(cfg.data['phens']+'/uniprot-organism__homo+sapiens_-filtered-proteome_UP000005640+AND+organi--.list', 'r') as f:
+    for line in f:
+        # add item to the list
+        line = re.sub(r'\n', '', line)
+        new_mobidb_list.append(line)
+
+# NDD
 ndd_subdf = pd.read_csv(cfg.data['phens-fdr'] + '/acc-phen-5percentFDR.csv')
 ndd_subdf = ndd_subdf.drop_duplicates()  # (4531, 3)
 ndd_pr_lst = ndd_subdf['acc'].unique().tolist()  # 1308 proteins  => these are all, not the selected phens
 ## brain
-brain_prot_lst = bd.brain_pr_lst_generator()  # n: 8297
-
+brain_prot_lst = bd.brain_pr_lst_generator()  # n: 8320
+## lists
 ndd_not_in_mobidb = list(set(ndd_pr_lst).difference(mobidb_pr_lst))
 brain_not_in_mobidb = list(set(brain_prot_lst).difference(mobidb_pr_lst))
-brain_not_in_mobi_cleaned = [x for x in brain_not_in_mobidb if str(x) != 'nan']
-# Writing to text
-with open(cfg.data['phens'] + '/ndd-not-in-mobidb.txt', 'w') as f:
-    for acc in ndd_not_in_mobidb:
-        f.write("%s\n" % acc)
 
-with open(cfg.data['phens'] + '/brain-not-in-mobidb.txt', 'w') as f:
-    for acc in brain_not_in_mobi_cleaned:
-        f.write("%s\n" % acc)
-new_df = mobidb.head(10)
-new_df.to_csv(cfg.data[''] + '/mobidb-head-10rows.csv')
+# checklist = list(set().difference(l1))
+
+#TODO: make separate class for this data and manage them
+# discard 11 proteins from everywhere, compare new mobidb(appended) with the list Damiano shared + your list of proteins and stuff
 
 # my_proteins_acc_lst = ['Q14204', 'P14867']
 # proteins_dict = {}
