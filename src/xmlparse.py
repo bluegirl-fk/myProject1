b7fbc65f9b2efb96f58723c1ae2fea40798b7bcd
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-09-14 17:16:21 +0200

    xml progress

--- src/xmlparse.py
+++ src/xmlparse.py
@@ -1,20 +1,34 @@
 # This file is created to parse uniprot xml data file to get mut locations and other useful info (sep 14th)
 import xml.etree.ElementTree as ET
-from inspect import getmembers, isclass, isfunction
 import config as cfg
 
+acc_protinfo_dic = {}
 NS = {'uniprot': 'http://uniprot.org/uniprot'}
 tree = ET.parse(cfg.data['xml'] + '/uniprot_example.xml')
 root = tree.getroot()
+# TODO: keep the length and compare with mobidb as a second checking measure of data accuracy
 sequence_length = int(root.find("uniprot:entry/uniprot:sequence", NS).attrib["length"])
-# for ele in root.findall("uniprot:entry/uniprot:dbReference", NS):
-#     if ele.attrib["type"] == "PDB":
-#         print(ele.attrib)
-#         for ele2 in ele.findall("uniprot:property", NS):
-#             if ele2.attrib["type"] == "chains":
-#                 print(ele2.attrib)
+entries = root.findall('uniprot:entry', NS)
+features = root.findall('uniprot:entry/uniprot:feature', NS)
 
-# for ele in root.findall('uniprot:entry/uniprot:feature', NS):  # try this with tree instead of NS
+# TODO: this code is not correct cuz it needs to append several seq variant data to rach acc which is dict key, and in the end the mut position will be added to the dict
+# maybe should use var id as keys of child dicts
+# data to be stored in the dictionary will be:
+# {acc: {var_id: {description: xxxx, evidence:xxxxx, position:x, orig_aa:x, var_aa:x}}}
+for entry in entries:
+    var_positions_lst = []
+    acc = (entry.find('uniprot:accession', NS)).text
+    acc_protinfo_dic[acc] = {}  # this will be changes later
+    for feature in features:
+        if feature.attrib['type'] == 'sequence variant':
+            acc_protinfo_dic[acc] = feature.attrib
+            for pos in feature.findall('uniprot:location/uniprot:position', NS):
+                var_positions_lst.append(pos.attrib['position'])
+            feature.attrib['var_position'] = var_positions_lst
+
+print(acc_protinfo_dic)
+
+# for ele in root.findall('uniprot:entry/uniprot:feature', NS):
 #     if ele.attrib['type'] == 'sequence variant':
 #         print(ele.attrib)
 #         for ele2 in ele.findall('uniprot:location/uniprot:position', NS):
