1dd080ccb5c3171df028877cfd02be2cbfbbb4cc
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-09-15 12:30:44 +0200

    needs debug

--- src/xmlparse.py
+++ src/xmlparse.py
@@ -2,7 +2,6 @@
 import xml.etree.ElementTree as ET
 import config as cfg
 
-acc_protinfo_dic = {}
 NS = {'uniprot': 'http://uniprot.org/uniprot'}
 tree = ET.parse(cfg.data['xml'] + '/uniprot_example.xml')
 root = tree.getroot()
@@ -10,34 +9,36 @@ root = tree.getroot()
 sequence_length = int(root.find("uniprot:entry/uniprot:sequence", NS).attrib["length"])
 entries = root.findall('uniprot:entry', NS)
 features = root.findall('uniprot:entry/uniprot:feature', NS)
-
+print(len(features)) #TODO check this length to see if includes both entries or what
 # maybe should use var id as keys of child dicts
 # data to be stored in the dictionary will be:
 # {acc: {var_id: {description: xxxx, evidence:xxxxx, position:x, orig_aa:x, var_aa:x}}}
 # (find a solution for entries without variant sequence data to be skipped) , in the end do it in terminal not pycharm
-
+acc_protinfo_dic = {}
+feature_tmp_dic = {}
 var_tmp_dict = {}
 for entry in entries:
     acc = (entry.find('uniprot:accession', NS)).text
-    acc_protinfo_dic[acc] = {}  # this will be changes later
+    feature_tmp_dic = {}
     for feature in features:
         if feature.attrib['type'] == 'sequence variant':
-            feature_tmp_dic = {}  # stores vars of a Pr, this child dict will be added to acc_protinfo parent dict
+            var_tmp_dict = {}
             var_id = feature.attrib['id']
             feature_tmp_dic[var_id] = {}
-            description = feature.attrib['description']
-            feature_tmp_dic[var_id].append(description)
+            var_tmp_dict['description'] = feature.attrib['description']
             # acc_protinfo_dic[acc] = feature.attrib
-            aa_orig = feature.find('uniprot:original', NS).text
-            feature_tmp_dic[var_id].append(aa_orig)
-            aa_var = feature.find('uniprot:variation', NS).text
-            feature_tmp_dic[var_id].append(aa_var)
+            var_tmp_dict['aa_orig'] = feature.find('uniprot:original', NS).text
+            var_tmp_dict['aa_var'] = feature.find('uniprot:variation', NS).text
             for pos in feature.findall('uniprot:location/uniprot:position', NS):
-                position = pos.attrib['position']
-                feature_tmp_dic[var_id].append(position)  # check int/str based on expand regions method and mobidb data
-    acc_protinfo_dic[acc] = feature_tmp_dic
+                var_tmp_dict['position'] = pos.attrib['position']  # check int/str based on expand regions method and mobidb data
+                feature_tmp_dic[var_id] = var_tmp_dict
 
-print(acc_protinfo_dic)
-print(len(acc_protinfo_dic))
-print(acc_protinfo_dic.keys())
-# add disease, then turn into a dataframe
+        acc_protinfo_dic[acc] = feature_tmp_dic
+    # print(acc_protinfo_dic)
+
+# print(acc_protinfo_dic)  # TODO: debug, shows all variant for all accs, does not separate each ACC vars
+# print(len(acc_protinfo_dic))
+# print(acc_protinfo_dic.keys())
+# print(acc_protinfo_dic['P01911'])
+# print(acc_protinfo_dic['P04439'])
+# # add disease, then turn into a dataframe
