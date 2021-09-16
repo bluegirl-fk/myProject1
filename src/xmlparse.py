f0408fa4e96a2914bc825a5230fd112da4354951
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-09-16 15:07:05 +0200

    modified if condition

--- src/xmlparse.py
+++ src/xmlparse.py
@@ -5,12 +5,11 @@ import pandas as pd
 import dateutil
 
 NS = {'uniprot': 'http://uniprot.org/uniprot'}
-tree = ET.parse(cfg.data['xml'] + '/uniprot_example.xml')
-# tree = ET.parse(cfg.data['xml'] + '/uniprot-reflst-20001to40000.xml')
+# tree = ET.parse(cfg.data['xml'] + '/uniprot_example.xml')
+tree = ET.parse(cfg.data['xml'] + '/uniprot-reflst-40001tilltheend.xml')
 root = tree.getroot()
 # sequence_length = int(root.find("uniprot:entry/uniprot:sequence", NS).attrib["length"])
 entries = root.findall('uniprot:entry', NS)
-# {acc: {var_id: {description: xxxx, evidence:xxxxx, position:x, orig_aa:x, var_aa:x}}}
 acc_protinfo = []
 for entry in entries:
     acc = (entry.find('uniprot:accession', NS)).text
@@ -18,39 +17,32 @@ for entry in entries:
         if feature.attrib['type'] == 'sequence variant':
             for pos in feature.findall('uniprot:location/uniprot:position', NS):
                 if 'description' in feature.attrib:
-                    print('inside the if')
-                    acc_protinfo.append([acc, feature.attrib['id'], feature.attrib['description'],
-                                         (feature.find('uniprot:original', NS)).text,
-                                         (feature.find('uniprot:variation', NS)).text,
-                                         pos.attrib['position']])
+                    if feature.find('uniprot:original', NS) is not None:
+                        print('inside the if')
+                        acc_protinfo.append([acc, feature.attrib['id'], feature.attrib['description'],
+                                             (feature.find('uniprot:original', NS)).text,
+                                             (feature.find('uniprot:variation', NS)).text,
+                                             pos.attrib['position']])
+                    else:
+                        print('Else!!!')
+                        acc_protinfo.append([acc, feature.attrib['id'], feature.attrib['description'],
+                                             None, None, pos.attrib['position']])
                 else:
-                    acc_protinfo.append([acc, feature.attrib['id'], None,
-                                         feature.get('original'), feature.find('./variation'), pos.attrib['position']])
+                    if feature.find('uniprot:original', NS) is not None:
+                        print('inside the if')
+                        acc_protinfo.append([acc, feature.attrib['id'], None,
+                                             (feature.find('uniprot:original', NS)).text,
+                                             (feature.find('uniprot:variation', NS)).text,
+                                             pos.attrib['position']])
+                    else:
+                        print('Else!!!')
+                        acc_protinfo.append([acc, feature.attrib['id'], None,
+                                             None, None, pos.attrib['position']])
 
-# with open(cfg.data['phens']+'/xml-parsed.txt', 'w') as f:
-#     for item in acc_protinfo:
-#         f.write("%s\n" % item)
 
 df = pd.DataFrame(acc_protinfo, columns=['acc', 'id', 'description', 'aa', 'variation', 'pos'])
-df.to_csv(cfg.data['phens'] + '/uniprot_variants.csv')
+df.to_csv(cfg.data['phens'] + '/uniprot_variants-parsed-40001tillend.csv')
+# TODO: check the final df against the ref lst and make sure all proteins are included, if not they should not be found in uniprot
+# TODO: make dict of acc and corresponding diseases
 
-#
-# print(acc_protinfo_dic)
-# acc_pos_d = {}
-# acc_lst = list(acc_protinfo_dic.keys())
-# for acc in acc_lst:
-#     vars_per_each_acc_lst = list(acc_protinfo_dic[acc].keys())
-#     muts_pos_per_each_acc_lst = []
-#     for id in vars_per_each_acc_lst:
-#         mut_position = acc_protinfo_dic[acc][id]['position']
-#         muts_pos_per_each_acc_lst.append(mut_position)
-#     acc_pos_d[acc] = [list(set(muts_pos_per_each_acc_lst))]
-#
-# print(acc_pos_d)
-# mutpos_df = pd.DataFrame(data=acc_pos_d)
-# mutpos_df = mutpos_df.T
-#
-#
-# # TODO: make dict of acc and corresponding diseases, for the ones not having that section, put None.
-# #  then do the mut in IDR analysis again
-# # in the end do it in terminal not pycharm
+
