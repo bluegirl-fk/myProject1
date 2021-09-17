ae8c0f1c8aca1ef3159db6bf6aa84321d7e52e61
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-09-17 17:13:23 +0200

    transferred todo

--- src/varAnalysis.py
+++ src/varAnalysis.py
@@ -1,2 +1,9 @@
 # this is to perform previous analysis which was done in mobibool and protanalyse but for 'variants in IDR' dataset
 # Sep 17th
+import pandas as pd
+import config as cfg
+# TODO: insert each of this in mobibool instead of mobidb, but you need to also delete ndd and brain that are not in
+#  this list maybe use left merge!
+
+mobidblite_inidr_df = pd.read_csv(cfg.data['vars'] + '/muts-in-IDR-mobidb_lite.csv')
+dismajority_inidr_df = pd.read_csv(cfg.data['vars'] + '/muts-in-IDR-disorder_majority.csv')
--- src/variants.py
+++ src/variants.py
@@ -70,5 +70,4 @@ lite_mut_in_df, dismaj_mut_in_df = in_idr_df_generator([mobidb_lite_mrg, disorde
 lite_mut_in_df.to_csv(cfg.data['vars'] + '/muts-in-IDR-mobidb_lite.csv')
 dismaj_mut_in_df.to_csv(cfg.data['vars'] + '/muts-in-IDR-disorder_majority.csv')
 
-# TODO: insert each of this in mobibool instead of mobidb, but you need to also delete ndd and brain that are not in
-#  this list maybe use left merge!
+
