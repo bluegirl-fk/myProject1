dd6159194526b6ce4e705b86d08210a92252929a
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-09-23 15:05:55 +0200

    filterer&percntage methods

--- src/varAnalysis.py
+++ src/varAnalysis.py
@@ -15,14 +15,14 @@ def vars_df_generator(input_df):
     # list, then these dfs will be used in mobibool or protanalysis
     var_pr_lst = input_df['acc'].unique().tolist()
     ## ndds in variants
-    ndd_in_idr = list(set(ndd_pr_lst).intersection(var_pr_lst))
-    ndd_in_idr_subdf = ndd_subdf[ndd_subdf.acc.isin(ndd_in_idr)]
-    del ndd_in_idr_subdf['Unnamed: 0']
+    ndd_in_var = list(set(ndd_pr_lst).intersection(var_pr_lst))
+    ndd_in_var_subdf = ndd_subdf[ndd_subdf.acc.isin(ndd_in_var)]
+    del ndd_in_var_subdf['Unnamed: 0']
     ## Brain
-    brn_in_idr_lst = list(set(brain_prot_lst).intersection(var_pr_lst))
-    brain_inidr_subdf = DataFrame(brn_in_idr_lst, columns=['acc'])
+    brain_var_lst = list(set(brain_prot_lst).intersection(var_pr_lst))
+    brain_var_subdf = DataFrame(brain_var_lst, columns=['acc'])
 
-    return input_df, brain_inidr_subdf, ndd_in_idr_subdf
+    return input_df, brain_var_subdf, ndd_in_var_subdf
 
 
 def all_vars_or_vars_inidr(input):
@@ -41,28 +41,37 @@ def all_vars_or_vars_inidr(input):
         return vars_df_generator(vars_in_idr_df)
 
 
+def df_feature_filterer(feature):  # this generates a filtered df with the desired mobidb feature filtered then it will
+    # be the input of var_idr_percentage_creator() method, input is the mobidb feature we want variants for
+    vars_all_features = pd.read_csv(cfg.data['vars'] + '/all-vars-mrg-mobidb-with-isin_idr-column.csv')
+    vars_my_feature = vars_all_features.loc[vars_all_features['feature'] == feature]
+    return vars_my_feature
+
+
+def var_idr_percentage_creator(df):  # a percentage column for variants in IDR / all Vars
+    # input it the out put of df_feature_filtered method
+    var_count_dic = (df.pivot_table(columns=['acc'], aggfunc='size')).to_dict()
+    # dict values to list so that I could append other data to dict values
+    for key, value in var_count_dic.items():
+        var_count_dic[key] = [value]
+    for k in var_count_dic:  # adds the count of each acc var being in idr
+        each_acc_vars_in_idr_count = df.loc[df['acc'] == k, 'isin_idr'].sum()
+        var_count_dic[k].append(each_acc_vars_in_idr_count)
+    # dict to df
+    var_count_df = pd.DataFrame.from_dict(var_count_dic, orient='index', columns=['total_vars', 'in_idr_vars'])
+    var_count_df['percentage'] = (var_count_df['in_idr_vars'] * 100) / var_count_df['total_vars']
+    var_count_df = var_count_df.reset_index()
+    var_count_df = var_count_df.rename(columns={'index': 'acc'})
+    mrg_var_and_percentage_df = pd.merge(df, var_count_df, on='acc')
+    mrg_var_and_percentage_df = mrg_var_and_percentage_df.drop(columns=['Unnamed: 0', 'Unnamed: 0_x', 'Unnamed: 0_y'])
+    return mrg_var_and_percentage_df
+
+
 ## NDD and brain original import
 ndd_subdf = pd.read_csv(cfg.data['phens-fdr'] + '/acc-phen-5percentFDR.csv')
 ndd_subdf = ndd_subdf.drop_duplicates()  # (4531, 3)
 ndd_pr_lst = ndd_subdf['acc'].unique().tolist()  # 1308 proteins
 brain_prot_lst = brn.brain_pr_lst_generator()  # n: 8320
 
-vars_all_features = pd.read_csv(cfg.data['vars'] + '/all-vars-mrg-mobidb-with-isin_idr-column.csv')
-vars_all_majority = pd.read_csv(cfg.data['vars'] + '/dismajority-vars-with-isin-column.csv')
-# print(vars_in_idr_majority['acc'].value_counts(dropna=False))
-# ndf = vars_all_majority[vars_all_majority.duplicated(subset=['acc'], keep=False)]
-acc_count_dict = (vars_all_majority.pivot_table(columns=['acc'], aggfunc='size')).to_dict()
-# dict values to list so that I could append other data to dict values
-for key, value in acc_count_dict.items():
-    acc_count_dict[key] = [value]
-for k in acc_count_dict: # adds the count of each acc var being in idr
-    each_acc_vars_in_idr_count = vars_all_majority.loc[vars_all_majority['acc'] == k, 'isin_idr'].sum()
-    acc_count_dict[k].append(each_acc_vars_in_idr_count)
-# dict to df
-acc_counts_df = pd.DataFrame.from_dict(acc_count_dict, orient='index', columns=['total_vars', 'in_idr_vars'])
-acc_counts_df['percentage'] = (acc_counts_df['in_idr_vars']*100)/acc_counts_df['total_vars']
-acc_counts_df.to_csv(cfg.data['vars'] + '/variants-count-and-hotspots.csv')
-
-
-
-
+mobidb_lite_var_count_df = var_idr_percentage_creator(df_feature_filterer('prediction-disorder-mobidb_lite'))
+mobidb_lite_var_count_df.to_csv(cfg.data['vars'] + '/mobidb-lite-idrvars-percentage.csv')
