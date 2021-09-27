540413dbce5399752d7d1b042c7231f8d3a78193
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-09-27 16:43:35 +0200

    reversed changees

--- src/varptm.py
+++ src/varptm.py
@@ -5,9 +5,14 @@ import config as cfg
 ptms_df = pd.read_csv(cfg.data['ptm'] + '/mobidb-parsed-ptm-info.csv', usecols=['acc', 'ptm_pos'])
 
 
-# disorder_maj = pd.read_csv(cfg.data['vars'] + '/disorder-majority-inout-idr-vars-count-normalized.csv')
-# dismaj_ptm_df = pd.merge(disorder_maj, ptms_df, on='acc')
-# del dismaj_ptm_df['Unnamed: 0']
+disorder_maj = pd.read_csv(cfg.data['vars'] + '/disorder-majority-inout-idr-vars-count-normalized.csv')
+dismaj_ptm_df = pd.merge(disorder_maj, ptms_df, on='acc')
+del dismaj_ptm_df['Unnamed: 0']
 
-ptms_df['ptm_pos'] = [','.join(map(str, l)) for l in ptms_df['ptm_pos']]
+for row in dismaj_ptm_df.iterrows():
+    for i in :
+        if int(row['position']) == i:
+            row['ptm_var'] = 1
+        else:
+            row['ptm_var'] = 0
 
