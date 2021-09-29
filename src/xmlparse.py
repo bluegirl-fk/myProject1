fe480db501a41a5ff176b212fe57dedc89a1ceba
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-09-29 15:14:08 +0200

    ptm xml parser working!

--- src/xmlparse.py
+++ src/xmlparse.py
@@ -47,27 +47,43 @@ def var_position_parser(xml_file_name):
 
 
 ## PTM parser
-NS = {'uniprot': 'http://uniprot.org/uniprot'}
-tree = ET.parse(cfg.data['xml'] + '/uniprot_example.xml')
-root = tree.getroot()
-entries = root.findall('uniprot:entry', NS)
-ptm_types = ['modified residue', 'glycosylation site', 'cross-link']  # disulfide bond not working
-acc_ptm_lst = []
-for entry in entries:
-    acc = (entry.find('uniprot:accession', NS)).text
-    for feature in entry.findall('uniprot:feature', NS):
-        for ptm_type in ptm_types:
-            if feature.attrib['type'] == ptm_type:
-                for pos in feature.findall('uniprot:location/uniprot:position', NS):
-                    acc_ptm_lst.append([acc, feature.attrib['description'], pos.attrib['position'], ptm_type])
-            elif feature.attrib['type'] == 'disulfide bond':
-                a = zip(feature.findall('uniprot:location/uniprot:begin', NS), feature.findall('uniprot:location/uniprot:end', NS))
-                for (begin, end) in a:
-                    acc_ptm_lst.append([acc, None, begin.attrib['position']+'&'+end.attrib['position'], 'disulfide bond'])
+def ptm_parser(file_name):
+    NS = {'uniprot': 'http://uniprot.org/uniprot'}
+    tree = ET.parse(cfg.data['xml'] + file_name)
+    root = tree.getroot()
+    entries = root.findall('uniprot:entry', NS)
+    ptm_types = ['modified residue', 'glycosylation site', 'cross-link']  # disulfide bond not working
+    acc_ptm_lst = []
+    for entry in entries:
+        acc = (entry.find('uniprot:accession', NS)).text
+        print(acc)
+        for feature in entry.findall('uniprot:feature', NS):
+            for ptm_type in ptm_types:
+                if feature.attrib['type'] == ptm_type:
+                    for pos in feature.findall('uniprot:location/uniprot:position', NS):
+                        acc_ptm_lst.append([acc, feature.attrib['description'], pos.attrib['position'], ptm_type])
+                # disulphide bond is out of the pts_types lst cuz had different position tags
+                elif feature.attrib['type'] == 'disulfide bond':
+                    disulfide_positions = zip(feature.findall('uniprot:location/uniprot:begin', NS),
+                                              feature.findall('uniprot:location/uniprot:end', NS))
+                    for (begin, end) in disulfide_positions:
+                        if 'position' in begin.attrib and 'position' in end.attrib:
+                            acc_ptm_lst.append(
+                                [acc, None, begin.attrib['position'] + '&' + end.attrib['position'], 'disulfide bond'])
+                        elif 'status' in begin.attrib:  # cuz not both of begin/end could be unknown at the same time
+                            acc_ptm_lst.append(
+                                [acc, None, begin.attrib['status'] + '&' + end.attrib['position'], 'disulfide bond'])
+                        elif 'status' in end.attrib:
+                            acc_ptm_lst.append(
+                                [acc, None, begin.attrib['position'] + '&' + end.attrib['status'], 'disulfide bond'])
 
-## todo add the end positionafter the begin, try to do both in one for loop, and concatenate them have them ass one value, eg: 46..79
-#todo put an if for ptms if they have description or no
 
 # df = pd.DataFrame(acc_ptm_lst, columns=['acc', 'description', 'pos', 'ptm_type'])
-# df.to_csv(cfg.data['ptm-u'] + '/uniprot_parsed-ptms-40001tilltheend.csv')
-# now I have two dfs to be concatenated into 1, but still lack the disulfide bond
+ptm1 = pd.read_csv(cfg.data['ptm-u'] + '/uniprot_parsed-ptms-1to40k.csv')
+ptm2 = pd.read_csv(cfg.data['ptm-u'] + '/uniprot_parsed-ptms-40001totheend.csv')
+del ptm1['Unnamed: 0']
+del ptm2['Unnamed: 0']
+
+ptm_df = ptm1.append(ptm2, ignore_index=True)
+ptm_df.to_csv(cfg.data['ptm-u'] + '/uniprot-ptms-all.csv')
+
