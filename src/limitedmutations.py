240ca0cc59d8a27c74019e29612a5f7c3d2645ca
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-10-15 17:17:53 +0200

    g4dn annot try in lmut

--- src/limitedmutations.py
+++ src/limitedmutations.py
@@ -22,10 +22,17 @@ def genes_lst_maker(fdr_limit):  # this is candidate genes from download page
     return positive_genes_lst
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
     # all_candidate_genes_df = pd.read_csv(cfg.data['gene4'] + '/limitedmut/Candidate_gene_1.2.txt',
     #                                      sep='\t')  # (124137, 12)
     ## Candidate genes from excel file (list is not complete)
     # genes_df = pd.read_excel(cfg.data['gene4'] + '/Gene.xlsx', engine='openpyxl')  # (8271, 13)
     # lst_1 = genes_lst_maker(0.05)  # 679 genes
     # lst_2 = genes_lst_maker(0.1)  # 0.02 = 1144, 0.1 = 842
+    cgenes = genes_lst_maker(0.05)
+    print(','.join(cgenes))
+    g4dn_annots = pd.read_csv(cfg.data['gene4'] + '/All_De_novo_mutations_and_annotations_1.2.txt',
+                              sep='\t', encoding='cp1252', low_memory=False)
+    candidate_annots = g4dn_annots.loc[g4dn_annots['Gene.refGene'].isin(cgenes)]
+    candidate_annots = candidate_annots.loc[candidate_annots['Func.refGene'] == 'exonic']
+    candidate_annots.to_csv(cfg.data['gene4'] + '/candidate_genes_annotations.csv')
