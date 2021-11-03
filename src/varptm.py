2f50fd32179199d742dcf75d75b4f3b169c33c62
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-11-03 16:16:27 +0100

    varptm disukfide divided

--- src/varptm.py
+++ src/varptm.py
@@ -56,30 +56,41 @@ def dismaj_var_in_ptm_df_generator():
     return ptm_checked_dismaj_df
 
 
-def ndd_idrvar_in_ptm_lst_df_generator(all_ptm_checked_pr_lst):
+def ndd_idrvar_in_ptm_lst_df_generator(all_ptm_checked_pr_lst, all_ptm_var_filtered_df):
     ndd_subdf = pd.read_csv(cfg.data['phens-fdr'] + '/acc-phen-5percentFDR.csv')
     # phens_lst = ['ASD', 'EE', 'ID', 'DD', 'SCZ', 'NDDs', 'Control']
     # ndd_subdf = ndd_subdf.loc[ndd_subdf.Phenotype.isin(phens_lst)]
     ndd_pr_lst = ndd_subdf['acc'].unique().tolist()  # 1308 proteins
     ptm_idr_var_pr_lst = list(set(ndd_pr_lst).intersection(all_ptm_checked_pr_lst))
     ptm_idr_var_ndd_subdf = ndd_subdf[ndd_subdf.acc.isin(ptm_idr_var_pr_lst)]
-    return ptm_idr_var_pr_lst, ptm_idr_var_ndd_subdf
+    ptm_idr_var_ndd_df = all_ptm_var_filtered_df.loc[all_ptm_var_filtered_df.acc.isin(ndd_pr_lst)]
+    return ptm_idr_var_pr_lst, ptm_idr_var_ndd_subdf, ptm_idr_var_ndd_df
+
+
+def ptm_divider(inputdf):
+    disulfide_bonds_df = inputdf.loc[inputdf['ptm_type'] == 'disulfide bond']
+    the_rest_of_ptms_df = inputdf.loc[inputdf['ptm_type'] != 'disulfide bond']
+    disulfide_bonds_pr_lst = disulfide_bonds_df['acc'].unique().tolist()
+    the_rest_of_ptms_pr_lst = the_rest_of_ptms_df['acc'].unique().tolist()
+    return disulfide_bonds_df, the_rest_of_ptms_df, disulfide_bonds_pr_lst, the_rest_of_ptms_pr_lst
 
 
 if __name__ == '__main__':
     # var_in_ptm_checked_df = dismaj_var_in_ptm_df_generator()
     var_in_ptm_checked_df = pd.read_csv(cfg.data['ptm-u'] + '/uniprot-vars-inptm-checked.csv')
     # var_in_ptm_df = var_in_ptm_checked_df.loc[var_in_ptm_checked_df['var_in_ptm'] == 1]  # (3170,12) vars, 527 Prs
-    ptm_idr_var_all_df = var_in_ptm_checked_df.loc[(var_in_ptm_checked_df['var_in_ptm'] == 1) &
-                                                   (var_in_ptm_checked_df['isin_idr'] == 1)]  # (366, 13)
-    ptm_idr_var_all_pr_lst = ptm_idr_var_all_df['acc'].unique().tolist()  # 177 Prs.
+    inptm_idr_var_all_df = var_in_ptm_checked_df.loc[(var_in_ptm_checked_df['var_in_ptm'] == 1) &
+                                                     (var_in_ptm_checked_df['isin_idr'] == 1)]  # (366, 13)
+    inptm_idr_var_all_pr_lst = inptm_idr_var_all_df['acc'].unique().tolist()  # 177 Prs.
     ## for NDDs
-    ptm_idr_var_ndd_pr_lst, _ = ndd_idrvar_in_ptm_lst_df_generator(ptm_idr_var_all_pr_lst)  # 16 contributes to 77 rows
+    in_ptm_idr_var_ndd_pr_lst, _, inptm_idr_var_ndd_df = ndd_idrvar_in_ptm_lst_df_generator(inptm_idr_var_all_pr_lst, inptm_idr_var_all_df)  # 16 contributes to 77 rows
     # in ndd phens col, meaning each pr is in charge of ~ 5 phens among all phens and not just my desired phenotypes
-    print('\n'.join(ptm_idr_var_ndd_pr_lst))
-    ptm_type_count = ptm_idr_var_all_df.groupby('ptm_type').count()
+    print('\n'.join(in_ptm_idr_var_ndd_pr_lst))
+    ptm_type_count = inptm_idr_var_all_df.groupby('ptm_type').count()
 
-    def ptm_type_seperator(inputdf):
-        ptms_disulfide_bonds_df = inputdf.loc[inputdf['ptm_type'] == 'disulfide bond']
-        the_rest_of_ptms_df = inputdf.loc[inputdf['ptm_type'] != 'disulfide bond']
-        return ptms_disulfide_bonds_df, the_rest_of_ptms_df
+
+
+
+
+    _, _, all_disulfide_pr_lst, all_other_ptms_pr_lst = ptm_divider(inptm_idr_var_all_df)  # 19 # 159
+    _, _, ndd_disulfide_pr_lst, ndd_other_ptms_pr_lst = ptm_divider(inptm_idr_var_ndd_df)  # 1  # 16
