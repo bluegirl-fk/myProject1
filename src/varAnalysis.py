6b06c48ab44769c6de686fd74c7b225628e8101c
fatemeh <fatemeh.kordevani@studenti.unipd.it>
2021-11-09 16:05:35 +0100

    difference heatmap

--- src/varAnalysis.py
+++ src/varAnalysis.py
@@ -118,43 +118,56 @@ def draw_barplot(x, y, data, xticklabel, yscale):  # input is DF, not list
     return
 
 
-def residue_heatmapper(df, hmap_title, filename):
+def residue_heatmapper(df_lst, hmap_title_lst, filename):
     # input df is disorder_majority or filtered_dis_maj if preferred
-    residue_pivot_df = heatmap_pivotdf_maker(df)
-    sns.set(font_scale=1.7)
+    new_pivot_df_lst = heatmap_pivotdf_maker(df_lst)
+    sns.set(font_scale=2.7)
+    fig, axes = plt.subplots(len(new_pivot_df_lst), 1, figsize=(30, 20 * len(new_pivot_df_lst)))
+    for i, (ax, d, t) in enumerate(zip(axes.reshape(-1), new_pivot_df_lst, hmap_title_lst)):
+        sb = sns.heatmap(d, cmap="viridis", annot=True, fmt='g', linewidth=0.5, ax=ax, square=True,
+                         cbar_kws={'label': 'Residue transition percentage'}
+                         )
+        ax.set_title(t, fontsize=40)
+        ax.set_xlabel('Variant residues', fontsize=30)
+        ax.set_ylabel('Original residues', fontsize=30)
+        ax.tick_params(axis='x', colors='red')
 
-
-
-    fig = plt.figure(figsize=(20, 15))
-    ax = plt.axes()
-    g = sns.heatmap(residue_pivot_df, linewidths=.5, cmap='viridis', annot=True, fmt='g', ax=ax, cbar_kws={'label': 'Residue transition percentage'})
-    ax.set_title(hmap_title, fontsize=40)
-    plt.xlabel('Variant residues', fontsize=25)
-    # ax.xaxis.label.set_color('red')
-    plt.ylabel('Original residues', fontsize=25)
-    # ax.yaxis.label.set_color('blue')
-    # tick label color
-    ax.tick_params(axis='x', colors='red')
+        if i < (len(new_pivot_df_lst) - 1):
+            sb.set(xticklabels=[])
+            sb.set(xlabel=None)
+    plt.tight_layout()
     plt.savefig(cfg.plots['var-hms'] + '/' + filename + '.png', dpi=120)
     plt.show()
-    # plt.savefig(cfg.plots['var-hms'] + '/' + filename + '.png', dpi=120)
-    return residue_pivot_df
 
 
-def heatmap_pivotdf_maker(df):
-    res_df = df[['acc', 'var_id', 'orig_aa', 'var_aa']]
-    aa_lst = res_df['orig_aa'].unique().tolist()
-    res_df = res_df.loc[res_df.var_aa.isin(aa_lst)]
-    residue_ser = res_df.groupby(['orig_aa', 'var_aa']).size().to_frame(name='size').reset_index()
-    residue_pivot_df = pd.pivot_table(residue_ser, values='size', index='orig_aa', columns='var_aa')
-    aa_categ_order_x = ['C', 'M', 'V', 'L', 'I', 'W', 'Y', 'F', 'H', 'T', 'N', 'Q', 'K', 'R', 'D', 'E', 'A', 'S', 'G',
-                        'P']
-    aa_categ_order_y = aa_categ_order_x.__reversed__()
-    residue_pivot_df = residue_pivot_df.reindex(columns=aa_categ_order_x)
-    residue_pivot_df = residue_pivot_df.reindex(aa_categ_order_y)
-    max_value = residue_pivot_df.max()
-    residue_pivot_df = residue_pivot_df.div(max_value).round(2) * 100
-    return residue_pivot_df
+    # ax.set_title(hmap_title, fontsize=40)
+    # plt.xlabel('Variant residues', fontsize=25)
+    # # ax.xaxis.label.set_color('red')
+    # plt.ylabel('Original residues', fontsize=25)
+    # # ax.yaxis.label.set_color('blue')
+    return
+
+
+def heatmap_pivotdf_maker(df_lst):
+    new_pivot_df_lst = []
+    for df in df_lst:
+        res_df = df[['acc', 'var_id', 'orig_aa', 'var_aa']]
+        aa_lst = res_df['orig_aa'].unique().tolist()
+        res_df = res_df.loc[res_df.var_aa.isin(aa_lst)]
+        residue_ser = res_df.groupby(['orig_aa', 'var_aa']).size().to_frame(name='size').reset_index()
+        residue_pivot_df = pd.pivot_table(residue_ser, values='size', index='orig_aa', columns='var_aa')
+        aa_categ_order_x = ['C', 'M', 'V', 'L', 'I', 'W', 'Y', 'F', 'H', 'T', 'N', 'Q', 'K', 'R', 'D', 'E', 'A', 'S', 'G',
+                            'P']
+        aa_categ_order_y = aa_categ_order_x.__reversed__()
+        residue_pivot_df = residue_pivot_df.reindex(columns=aa_categ_order_x)
+        residue_pivot_df = residue_pivot_df.reindex(aa_categ_order_y)
+        max_value = residue_pivot_df.max()
+        residue_pivot_df = residue_pivot_df.div(max_value).round(2) * 100
+        residue_pivot_df = residue_pivot_df.apply(pd.to_numeric)
+        new_pivot_df_lst.append(residue_pivot_df)
+    difference_df = new_pivot_df_lst[0].subtract(new_pivot_df_lst[1])
+    new_pivot_df_lst.append(difference_df)
+    return new_pivot_df_lst
 
 if __name__ == '__main__':
 
@@ -176,11 +189,12 @@ if __name__ == '__main__':
     # 4631 unique ACCs
     mobilite_vars_in = mobidb_lite.loc[mobidb_lite['isin_idr'] == 1]
     mobilite_vars_out = mobidb_lite.loc[mobidb_lite['isin_idr'] == 0]
-    residue_heatmapper(mobilite_vars_in, 'Residue Variations - in disordered region', 'mobilite_vars_in')
-    residue_heatmapper(mobilite_vars_out, 'Residue Variations- in ordered region', 'mobilite_vars_out')
-    # for ndds
     ndd_mobilite_vars_in = mobilite_vars_in.loc[mobilite_vars_in.acc.isin(ndd_pr_lst)]
     ndd_mobilite_vars_out = mobilite_vars_out.loc[mobilite_vars_out.acc.isin(ndd_pr_lst)]
-    residue_heatmapper(ndd_mobilite_vars_in, 'Residue Variations(NDDs) - in disordered region', 'ndd-mobilite_vars_in')
-    residue_heatmapper(ndd_mobilite_vars_out, 'Residue Variations(NDDs)- in ordered region', 'ndd-mobilite_vars_out')
+    residue_heatmapper([ndd_mobilite_vars_in, mobilite_vars_in], ['Residue Variations - in IDRs (NDDs)', 'Residue Variations- in IDRs (Homo sapiens)', 'Difference (NDD - Homo sapiens)'], 'test1')
+    # residue_heatmapper(mobilite_vars_out, 'Residue Variations- in ordered region', 'mobilite_vars_out')
+    # for ndds
+
+    # residue_heatmapper(ndd_mobilite_vars_in, 'Residue Variations(NDDs) - in disordered region', 'ndd-mobilite_vars_in')
+    # residue_heatmapper(ndd_mobilite_vars_out, 'Residue Variations(NDDs)- in ordered region', 'ndd-mobilite_vars_out')
 
